814aaf89f8887d351e6d23a29de49702b99b881f
Lesnov_Art <lesnovart88@gmail.com>
2021-01-20 20:26:55 +0200

    add prettier, ts, tsconfig

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Йоу!</Text>
+      <Text>photo-app</Text>
       <StatusBar style="auto" />
     </View>
   );
