999f4574042e09a05e6c1ce428610b91ffb68546
Lesnov_Art <lesnovart88@gmail.com>
2021-01-20 22:06:12 +0200

    add BottomTabNavigator

---
+++ src/navigation/DrawerNavigator.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { BottomTabNavigator } from "./BottomTabNavigator";
+import { SideDrawer } from "../ui/SideDrawer";
+
+const Drawer = createDrawerNavigator();
---
+++ src/navigation/Routes.ts
@@ -0,0 +1,4 @@
+export enum Routes {
+  PhotosFeed = "Photos",
+  Settings = "settings"
+}
