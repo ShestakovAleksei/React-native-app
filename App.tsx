d0f961ae6c2ccd8cc0b6b51c57b36b1329ec61b6
Oksana Kushchik <okushchik@gmail.com>
2021-01-20 23:57:32 +0200

    add axios

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import  {Api}  from './src/api/Api';
+
+
 
 export default function App() {
+
+  console.log(Api())
+
+
   return (
     <View style={styles.container}>
-      <Text>photo-app</Text>
+      <Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
